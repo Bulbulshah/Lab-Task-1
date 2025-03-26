874c43bb2aa380bb860aefa15ddd781480e59e73
Bulbulshah <bulbulgojo@gmail.com>
2025-03-27 00:05:45 +0500

    Lab Tasks

---
+++ Lab task 05/Lab task 5/Question_5.java
@@ -0,0 +1,29 @@
+public class Question_5 {
+    class Person {
+        final String name; 
+    
+        Person(String name) {
+            this.name = name;
+        }
+        void changeName(String newName) {
+            // this.name = newName; //
+        }
+    
+        void display() {
+            System.out.println("Name: " + name);
+        }
+    }
+    
+    public static void main(String[] args) {
+        
+        Person p1 = new Person("bulbul");
+        p1.display();
+        
+        
+        
+        p1.changeName("Shahani"); 
+    }
+}
+        
+           
+
